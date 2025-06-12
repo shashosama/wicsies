b70dee8290bb7a99d64e6fe4b7d5c7b80d259d37
shashosama <160487546+shashosama@users.noreply.github.com>
2025-06-12 01:11:20 +0000

    app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LandingPage"; 
 import LandingPage from "./pages/LandingPage";
+import SurveyPage from "./pages/surveyPage";
 // (others: SurveyPage, ResultsPage, etc.)
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
 
         {/* Add next pages later: */}
-        {/* <Route path="/survey" element={<SurveyPage />} /> */}
+        { <Route path="/survey" element={<SurveyPage />} /> }
       </Routes>
     </BrowserRouter>
   );
