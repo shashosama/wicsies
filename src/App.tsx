4ebf9ee6efe84462801af7e71baa093837f22ae4
shasho <160487546+shashosama@users.noreply.github.com>
2025-06-28 10:47:22 -0700

    Fix missing .tsx file extensions in imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import SurveyPage from "./pages/SurveyPage";
-import ResultPage from "./pages/ResultPage";
+import LandingPage from "./pages/LandingPage.tsx";
+import SurveyPage from "./pages/SurveyPage.tsx";
+import ResultPage from "./pages/ResultPage.tsx";
 
 function App() {
   return (
